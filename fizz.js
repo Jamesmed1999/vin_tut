dc4ac0bfd0a71f6d2445c8abb5c74a8852f468bd
Jamesmed1999 <74335705+Jamesmed1999@users.noreply.github.com>
2023-05-07 19:18:03 -0400

    fizbuzz lol

--- fizz.js
+++ fizz.js
@@ -1,16 +1,13 @@
-let n = 50;
+let n = 100;
 for(let i =1; i<n+1; i++)
 {
 let res =""
 
-if(i%3 == 0)
-{
-    res += "fizz"
-}
-if(i%5 == 0)
-{
-    res += "buzz"
-}
+if(i%3 == 0) {res += "fizz"}
+
+if(i%5 == 0) {res += "buzz"}
+
 if(res =="" ) { res = i}
+
 console.log(res)
 }
